ad8b50b1ea0bdd01b13d3d9a659a62e12b861c40
이건주 <dlrjswn8747@nton.co.kr>
2024-08-20 16:05:43 +0900

    메인페이지 완성

--- src/pages/main.js
+++ src/pages/main.js
@@ -7,6 +7,9 @@ function Main(){
         <div className="sTit">LeeGeonJu</div>
         <div className="mTit">PORTFOLIO</div>
         <div className="txt">탄탄한 구조의 코드위에 편안함과 기능성을 얹는 이건주입니다.<br/>최신식 트렌트와 UI/UX중심의 퍼블리싱을 하고있습니다.<br/>지금은 프론트영역에도 많은 관심이 있어 발전중입니다.</div>
+
+        <div className="scrollIcon">
+        </div>
       </section>
     </main>
   );
